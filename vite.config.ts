c792b82a5b9679aa959f2aeb9491663fd7bd996b
Nguyen Ngoc Khanh <ws.ngkhanh@gmail.com>
2025-09-06 20:24:55 +0700

    update vite.config.js và package.json để deploy gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/conghthanh.PIS",
+  base: "/congthanh.PIS",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
